41a5dfd73a22046899065ac50d75eea34574aac3
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-18 19:01:02 +0300

    Интеграция с TelegramBot работает

--- telegram_integration/views.py
+++ telegram_integration/views.py
@@ -1,19 +1,22 @@
 # telegram_integration/views.py
 
 import os
-import sys
-
 import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
 import logging
 from django.http import HttpResponse
 from telegram import Bot, Update
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
-
-# Установка оптимального размера пула соединений для Telegram API
-import requests
-from requests.adapters import HTTPAdapter
+from django.contrib.auth.models import User
+from users.models import UserProfile  # Импорт модели UserProfile
 
 TELEGRAM_BOT_TOKEN = '6508959358:AAESl7Sb20VbkYx26qU-T0piY0UF_EeiWf8'
+
+# Устанавливаем оптимальный размер пула соединений для Telegram API
+import requests
+from requests.adapters import HTTPAdapter
 
 
 class TelegramSession(requests.Session):
@@ -22,47 +25,42 @@ class TelegramSession(requests.Session):
         self.mount('https://api.telegram.org', HTTPAdapter(pool_connections=8, pool_maxsize=8))
 
 
+# Инициализируем нашу сессию
 requests = TelegramSession()
 
-# Указание пути к файлу с настройками Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-# Инициализация Django
-django.setup()
-
-# Импорт модели UserProfile после инициализации Django
-from users.models import UserProfile
-
-# Инициализация объекта logger
+# Инициализируем логгер
 logger = logging.getLogger(__name__)
 
 
-# Определение функции для обновления профиля пользователя
+# Функция для обновления профиля пользователя
 def update_user_profile(user_id, chat_id):
     try:
-        user_profile = UserProfile.objects.get(user_id=user_id)
-        # Если профиль уже существует, обновим telegram_chat_id
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        user = User.objects.create(id=user_id)
+
+    user_profile, created = UserProfile.objects.get_or_create(
+        user=user,
+        defaults={'telegram_chat_id': chat_id}
+    )
+
+    if not created:
         user_profile.telegram_chat_id = chat_id
         user_profile.save()
-    except UserProfile.DoesNotExist:
-        # Если профиля нет, создадим новый
-        user_profile = UserProfile.objects.create(user_id=user_id, telegram_chat_id=chat_id)
 
     return user_profile
 
 
-# Определение функции для команды /start
+# Функция для команды /start
 def start(update: Update, context):
-    user_id = update.effective_user.id  # Получаем идентификатор пользователя
-    chat_id = update.effective_chat.id  # Получаем идентификатор чата
-
+    user_id = update.effective_user.id
+    chat_id = update.effective_chat.id
     user_profile = update_user_profile(user_id, chat_id)
-
     context.bot.send_message(chat_id=chat_id,
-        text='Добро пожаловать в Habit Tracker! Вы будете получать напоминания о ваших привычках здесь.')
+                             text='Добро пожаловать в Habit Tracker! Вы будете получать напоминания о ваших привычках здесь.')
 
 
-# Функция, выполняемая при возникновении ошибки
+# Функция для обработки ошибок
 def error(update: Update, context):
     logger.warning(f'Обновление {update} вызвало ошибку {context.error}')
 
@@ -84,21 +82,15 @@ def webhook(request):
 
 # Запуск бота, если файл запущен как скрипт
 if __name__ == "__main__":
-    # Добавляем эту строку для правильной инициализации Django
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
-
     bot = Bot(token=TELEGRAM_BOT_TOKEN)
     updater = Updater(bot=bot, use_context=True)
     dp = updater.dispatcher
 
-    # Добавление обработчиков команд и сообщений
+    # Добавляем обработчики команд и сообщений
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Обработка ошибок
     dp.add_error_handler(error)
 
-    # Запуск обновлений
+    # Запускаем обновления
     updater.start_polling()
     updater.idle()
