0e8f5fae6344989775ed61a9c3f5d97680796d6c
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-21 11:41:15 +0300

    Добавлено поле - name habit_tracker/models.py модели Habit

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -13,6 +13,7 @@ from .validators import (
 
 class Habit(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habits')
+    name = models.CharField(max_length=100)
     place = models.CharField(max_length=255)
     time = models.TimeField()
     action = models.CharField(max_length=255)
