9a04b82059a03ad476522a05210b848606ba8e97
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-03-13 13:59:58 +0300

    Эндпоинты импортирую в главный url

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,5 @@
 # config/urls.py
 from users.views import RegisterUserView
-from habit_tracker.views import (
-    PublicHabitListView,
-    UserHabitListView,
-    HabitCreateView,
-    HabitUpdateView,
-    HabitDeleteView,
-)
 from django.contrib import admin
 from django.urls import path, include
 from telegram_integration.views import webhook
@@ -19,16 +12,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from habit_tracker.urls import urlpatterns as habit_tracker_urls
 
-
-# URL-пути для API
-api_urlpatterns = [
-    path("public-habits/", PublicHabitListView.as_view(), name="public-habits"),
-    path("my-habits/", UserHabitListView.as_view(), name="my-habits"),
-    path("create-habit/", HabitCreateView.as_view(), name="create-habit"),
-    path("update-habit/<int:pk>/", HabitUpdateView.as_view(), name="update-habit"),
-    path("delete-habit/<int:pk>/", HabitDeleteView.as_view(), name="delete-habit"),
-]
 
 # URL-пути для аутентификации и пользователей
 auth_urlpatterns = [
@@ -39,8 +24,8 @@ auth_urlpatterns = [
 ]
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include((api_urlpatterns, "habit_tracker"))),  # пространство имен "habit_tracker"
+    path('admin/', admin.site.urls),
+    path('api/', include(habit_tracker_urls)),  # Используем импортированные эндпоинты
     path("api/", include(auth_urlpatterns)),  # подключаем URL-пути для аутентификации и пользователей
     path("telegram/webhook/", webhook, name="telegram_webhook"),
     # Добавленные эндпоинты для документации
